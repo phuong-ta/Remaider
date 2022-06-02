1ffb1abf0a85e4f7091893e8847deead2624d5af
phuong-ta <phuong-ta@users.noreply.github.com>
2022-06-02 21:36:59 +0300

    struct with array

--- library/remaider.c
+++ library/remaider.c
@@ -1,48 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "currentTime.c"
- 
+
+// struct to get user remain
+ struct getRemaider {
+    int hours, minutes;
+    char name[20];
+};
+
 // Return multiple values from a function in C
-int remaider(void)
-{
-    int h, m,s, hR, mR;
-    int conditional =1;
+int remaider(void){
+    int h, m,s;
+    struct getRemaider gR[2];
+
+    // user give info
+    printf("Enter 2 remaiders: \n");
+    for (int i = 0; i < 2; i++) {
+        printf("Enter content: ");
+        scanf("%s", gR[i].name);
+        printf("Set hours: ");
+        scanf("%d", &gR[i].hours);
+        printf("Set minute: ");
+        scanf("%d", &gR[i].minutes);
+        
+    }
+
+    // run clock and remain 
     while (1){
         system("clear");
+        // get current time
         struct TuplePhuong t = getCurrentTime();
         h = t.hour;
         m = t.minute;
         s = t.second;
         printf("%d: %d: %d \n", h, m , s);
         fflush(stdout);
-        if (conditional==1){
-            printf("Set hours!");
-            scanf("%d", &hR);
-            printf("Set minuute!");
-            scanf("%d", &mR);
-            conditional=0;
+        // check remainder        
+        for (int i = 0; i < 2; i++) {
+            if (gR[i].hours==h && gR[i].minutes == m){
+                printf("%s\n",gR[i].name);
+            }
         }
-        if (hR==h && mR == m){
-        printf("remaider!!!!\n");
-    }
         system("sleep 1"); 
-    }
-    
- 
-
-
-    
+}
     return 0;
 }
-/*
-            if (conditional==1){
-            printf("Set hours!");
-            scanf("%d", &hR);
-            printf("Set minuute!");
-            scanf("%d", &mR);
-            conditional=0;
-        }
-        if (hR==h && mR == m){
-        printf("remaider!!!!");
-    }
-*/
